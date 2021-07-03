24ef7d58d1e5bfb8a670fe04b83191f2a9680a5a
Colton Acosta <cacost12@asu.edu>
2021-07-02 18:19:59 -0700

    Started blinkLED program

---
+++ blinkLED/blink.c
@@ -0,0 +1,11 @@
+/************************************************************* 
+* blinkLED -- Program that uses the STM32's GPIO to blink an *
+*             LED to test the status of the microcontroller  *
+*             programmer                                     *
+*                                                            * 
+* Author: Colton Acosta, cacost12@asu.edu                    *
+*                                                            * 
+* Date: 7/2/2021                                             *
+*************************************************************/ 
+
+
